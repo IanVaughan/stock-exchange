2d583b7bb01a6bb2d9469dee03b8b8f76d5f6dd6
Ian Vaughan <github@ianvaughan.co.uk>
2014-09-04 08:26:21 +0100

    Move converting data from csv file into CsvParser

--- csv_parser.rb
+++ csv_parser.rb
@@ -5,9 +5,19 @@ class CsvParser
     [].tap do |points|
       CSV.foreach(filename) do |row|
         unless row.any? {|r| r == '#N/A'}
-          points << Point.new(row)
+          points << Point.new(extract_data(row))
         end
       end
     end.sort_by { |p| p.date }
+  end
+
+  def self.extract_data(raw_row)
+    raw_date, *raw_numbers = raw_row
+    [convert_date(raw_date), *raw_numbers.map(&:to_f)]
+  end
+
+  def self.convert_date raw_date
+    month, day, year = raw_date.split('/').map(&:to_i)
+    Date.new(year, month, day)
   end
 end
