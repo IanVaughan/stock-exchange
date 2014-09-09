8d47a729a6c1b3665e8223c5bdc8203d4158e99f
Ian Vaughan <github@ianvaughan.co.uk>
2014-09-09 23:04:50 +0100

    Use points to match specs

--- spec/analiser_spec.rb
+++ spec/analiser_spec.rb
@@ -7,38 +7,36 @@ describe Analiser do
     before { subject.run }
 
     context 'finding the start point' do
-      let(:count) { 6 }
-      let(:start_point) { Point.new(position: 5).to_h }
-      it { expect(subject.start_point.to_h).to eq(start_point) }
+      let(:count) { 10 }
+      let(:start_point) { subject.points[4] }
+      it { expect(subject.start_point).to eq(start_point) }
     end
 
     context 'finding the end point' do
       let(:count) { 10 }
-      let(:start_point) { Point.new(position: 5).to_h }
-      let(:end_point) { Point.new(position: 7).to_h }
-      it { expect(subject.start_point.to_h).to eq(start_point) }
-      it { expect(subject.end_point.to_h).to eq(end_point) }
+      let(:end_point) { subject.points[6] }
+      it { expect(subject.end_point).to eq(end_point) }
     end
 
     context 'creates T1' do
       let(:count) { 10 }
-      let(:start_point) { Point.new(position: 5).to_h }
-      let(:end_point) { Point.new(position: 7).to_h }
-      it { expect(subject.trend.map(&:to_h)).to eq([start_point, end_point]) }
+      let(:start_point) { subject.points[4] }
+      let(:end_point) { subject.points[6] }
+      it { expect(subject.trend).to eq([start_point, end_point]) }
     end
 
     context 'creates T2' do
       let(:count) { 15 }
-      let(:start_point) { Point.new(position: 7).to_h }
-      let(:end_point) { Point.new(position: 15).to_h }
-      it { expect(subject.trend.map(&:to_h)).to eq([start_point, end_point]) }
+      let(:start_point) { subject.points[6] }
+      let(:end_point) { subject.points[14] }
+      it { expect(subject.trend).to eq([start_point, end_point]) }
     end
 
     context 'creates T3' do
       let(:count) { 20 }
-      let(:start_point) { Point.new(position: 7).to_h }
-      let(:end_point) { Point.new(position: 17).to_h }
-      it { expect(subject.trend.map(&:to_h)).to eq([start_point, end_point]) }
+      let(:start_point) { subject.points[6] }
+      let(:end_point) { subject.points[16] }
+      it { expect(subject.trend).to eq([start_point, end_point]) }
     end
   end
 
