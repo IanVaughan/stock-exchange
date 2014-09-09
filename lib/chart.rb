2db9ce3900cf4601966f83482fbe00d89bb38bbf
Ian Vaughan <github@ianvaughan.co.uk>
2014-09-09 19:29:15 +0100

    update runner after rewrite

--- lib/chart.rb
+++ lib/chart.rb
@@ -42,4 +42,30 @@ class Chart
       hash[_labels.count-1] = _labels.last
     end
   end
+
+  def self.chart_trend(last, line)
+    chart = Chart.new
+    chart.labels(points.first(line.count).map { |p| p.date.to_s }, 4)
+    %i{ mov_avg_20d mov_avg_50d px_high }.each do |plot|
+      chart.data(plot, points.first(line.count).map(&plot))
+    end
+    chart.data('last', last)
+    chart.data('start_end', line)
+    #chart.data('up', points.map { |p| p.uptrend? ? p.mov_avg_20d : nil })
+    chart.write("output/#{line.count}.png")
+  end
+
+  def self.create_trend_line(p1, p2)
+    [
+      [nil] * (p1.position-1),
+      plot_gradient(p1, p2),
+      p2.px_high
+    ].flatten
+  end
+
+  def self.plot_gradient(p1, p2)
+    times = (p2.position - p1.position)
+    inc = (p2.px_high - p1.px_high) / times
+    [].tap {|a| times.times {|t| a << p1.px_high + (inc * t) }}
+  end
 end
