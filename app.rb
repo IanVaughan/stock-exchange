36b75fcdccbf42ca488c483817c12c72026f5efd
Ian Vaughan <github@ianvaughan.co.uk>
2014-11-01 16:59:55 +0000

    better

--- app.rb
+++ app.rb
@@ -1,14 +1,51 @@
 require 'sinatra'
 require './lib/shares'
 
-get '/' do
-  filename = './data/sx5e_index.csv'
-  points = CsvParser.parse(filename)
-  # points = points[0..200]
-  top_points = TopPointDetection.new(points)
-  top_points.run(4)
-  erb :index, locals: {
-    samples: points.map(&:to_chart),
-    top_points: top_points.high_points.map(&:to_chart)
-  }
+use Rack::Session::Pool
+
+class App < Sinatra::Application
+
+  configure :production, :development do
+    enable :logging
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/load' do
+    points = CsvParser.parse(filename)
+    session[:points] = points
+    redirect '/'
+  end
+
+  get '/graph' do
+    points = session[:points]
+
+    if points.nil?
+      session[:flash] = 'no data!'
+      return
+    end
+
+    ww = (params['window_width'] || 4).to_i
+    gua = (params['give_up_after'] || 50).to_i
+    tops = top_points(points, ww, gua)
+
+    erb :graph, locals: {
+      window_width: ww,
+      give_up_after: gua,
+      samples: points.map(&:to_chart),
+      tops: tops
+    }
+  end
+end
+
+def filename
+  './data/sx5e_index.csv'
+end
+
+def top_points(points, window_width, give_up_after)
+  tpd = TopPointDetection.new(points)
+  tpd.run(window_width, give_up_after)
+  tpd.high_points.map(&:to_chart)
 end
