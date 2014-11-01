2ed983ae48b1c77501ec56914ce5f6f10270971f
Ian Vaughan <github@ianvaughan.co.uk>
2014-11-01 21:11:36 +0000

    return collections of count tops

--- lib/top_point_detection.rb
+++ lib/top_point_detection.rb
@@ -6,18 +6,22 @@ class TopPointDetection
     @points = points
   end
 
-  def run(window_width, give_up_after)
+  def run(window_width, give_up_after, collection_count)
     @window_width = window_width
     @give_up_after = give_up_after
+    @collection_count = collection_count
+
     @high_points = []
+    @collections = []
     points.each { |p| analise(p) }
   end
 
   private
-  attr_reader :points, :window_width, :high_point
+  attr_accessor :points, :window_width, :high_point, :collection
 
   def analise(p)
     @high_point ||= p
+    @collection ||= []
 
     # Find next higher point
     if p.px_high > @high_point.px_high
@@ -28,9 +32,12 @@ class TopPointDetection
     # Save found higher point
     if @new_high && (p.position - @high_point.position) >= window_width
       @high_points << @high_point
+      @collection << high_point
       @new_high = false
 
-      if @high_points.count % 3 == 0
+      if @high_points.count % collection_count == 0
+        @collections << collection
+        @collection = []
         @high_point = nil
       end
     end
@@ -38,6 +45,7 @@ class TopPointDetection
     # Give up finding higher point after a while
     if @high_point && (p.position - @high_point.position) >= @give_up_after
       @high_point = nil
+      @collection = []
     end
   end
 end
--- spec/top_point_detection_spec.rb
+++ spec/top_point_detection_spec.rb
@@ -4,11 +4,13 @@ describe TopPointDetection do
   let(:number_of_points) { 0 }
   let(:window_width) { 2 }
   let(:give_up_after) { 10 }
+  let(:collection_count) { 3 }
+
   let(:data) { load_test_input.first(number_of_points) }
   subject { described_class.new(data) }
 
   context 'top point detection' do
-    before { subject.run(window_width, give_up_after) }
+    before { subject.run(window_width, give_up_after, collection_count) }
 
     context 'finds top points' do
       COUNT_START_POSITION = {
@@ -24,6 +26,28 @@ describe TopPointDetection do
           let(:number_of_points) { c }
           let(:samples) { p.map {|i| data[i-1] } }
           it { expect(subject.high_points).to eq(samples) }
+        end
+      end
+    end
+  end
+
+  context 'top point collections' do
+    before { subject.run(window_width, give_up_after, collection_count) }
+
+    context 'finds top points' do
+      COUNT_START_POSITION = {
+        8 => [],
+        11 => [],
+        12 => [],
+        20 => [[5,10,17]],
+        25 => [[5,10,17]],
+      }
+
+      COUNT_START_POSITION.each do |c, p|
+        context "with the first #{c} points" do
+          let(:number_of_points) { c }
+          let(:samples) { p.map {|a| a.map {|i| data[i-1] }} }
+          it { expect(subject.collections).to eq(samples) }
         end
       end
     end
