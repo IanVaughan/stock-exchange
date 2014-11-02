9cb8ac157405c10ab0f79094925e7aa18a23187b
Ian Vaughan <github@ianvaughan.co.uk>
2014-11-02 22:12:34 +0000

    load more data in specS

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,15 +7,19 @@ require './lib/top_point_detection.rb'
 require './lib/trend'
 require 'pry'
 
-def load_test_input(filename = './spec/test.csv')
+def load_test_input(file = 'test.csv')
+  filename = "./spec/#{file}"
   Sample.reset_count
 
   [].tap do |points|
     CSV.foreach(filename) do |row|
       next if row.first == "Sample"
       data = {
-        date: Date.today+points.count,
-        px_high: row[1].to_i
+        date: Date.today + points.count,
+        px_high: row[1].to_i,
+        px_low: row[2].to_i,
+        mov_avg_50d: row[3].to_i,
+        mov_avg_20d: row[4].to_i,
       }
       points << Sample.new(data)
     end
