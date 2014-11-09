357422d780454c26b7d3c3fbe440448e97ef3c13
Ian Vaughan <github@ianvaughan.co.uk>
2014-11-09 09:44:07 +0000

    Gann angle

--- lib/gann_angle.rb
+++ lib/gann_angle.rb
@@ -1,21 +1,46 @@
 class GannAngle
-  attr_reader :start_point
+  attr_reader :days_before_startpoint, :a_b_window
+  attr_reader :start_point, :end_point
+  attr_reader :point_a, :point_b
+  attr_reader :alpha, :beta, :gamma
 
   def initialize(points)
     @points = points
   end
 
-  def run(days_before_startpoint)
-    @finding_start_point ||= true
-    points.each { |p| analise(p) }
+  def run(days_before_startpoint, a_b_window)
+    @days_before_startpoint = days_before_startpoint
+    @a_b_window = a_b_window
+
+    reset
+
+    points.each do |p|
+      unless find_start_point(p)
+        @point_a = @points[start_point.position - days_before_startpoint]
+
+        unless find_end_point(p)
+          # 3. Lowest low point between p1 and p2, mark as B
+          @point_b = lowest(@points[start_point.position..end_point.position])
+
+          # 4. Test that B - A > 90 days, ignore if not
+          if point_b.position - point_a.position < a_b_window
+            reset
+          else
+            # 5. Calculate price difference between A and B
+            @alpha, @beta, @gamma = calc_price_diff
+          end
+        end
+      end
+    end
   end
 
   private
 
   attr_reader :points
 
-  def analise(p)
-    find_start_point(p)
+  def reset
+    @finding_start_point = true
+    @finding_end_point = true
   end
 
   def find_start_point(p)
@@ -23,5 +48,31 @@ class GannAngle
       @start_point = p
       @finding_start_point = false
     end
+    @finding_start_point
+  end
+
+  def find_end_point(p)
+    if @finding_end_point && p.mov_avg_20d > p.mov_avg_50d
+      @end_point = p
+      @finding_end_point = false
+    end
+    @finding_end_point
+  end
+
+  def lowest(points)
+    points.sort_by!(&:px_low).first
+  end
+
+  def calc_price_diff
+    # 5. Calculate price difference between A and B, call this “y”
+    y = point_a.px_high.to_f - point_b.px_low.to_f
+    # 6. Calculate number of trading days between A and B, call this “x”
+    x = point_b.position.to_f - point_a.position.to_f
+
+    alpha = y / x
+    beta = (y * 0.618) / x
+    gamma = (y * 0.382) / x
+
+    [alpha, beta, gamma]
   end
 end
--- spec/gann_angle_spec.rb
+++ spec/gann_angle_spec.rb
@@ -1,15 +1,38 @@
 require 'spec_helper'
 
 describe GannAngle do
-  let(:days_before_startpoint) { 4 }
+  let(:days_before_startpoint) { 3 }
+  let(:a_b_window) { 5 }
 
   let(:data) { load_test_input('gann_data.csv').first(number_of_points) }
   subject { described_class.new(data) }
 
-  before { subject.run(days_before_startpoint) }
+  before { subject.run(days_before_startpoint, a_b_window) }
 
   context 'finds crossover point' do
     let(:number_of_points) { 6 }
     it { expect(subject.start_point).to eq(data[3]) }
+  end
+
+  context 'finds point A' do
+    let(:number_of_points) { 6 }
+    it { expect(subject.point_a).to eq(data[1]) }
+  end
+
+  context 'finds crossback point' do
+    let(:number_of_points) { 19 }
+    it { expect(subject.end_point).to eq(data[18]) }
+  end
+
+  context 'finds point B' do
+    let(:number_of_points) { 19 }
+    it { expect(subject.point_b).to eq(data[12]) }
+  end
+
+  context 'calcs stuff' do
+    let(:number_of_points) { 19 }
+    it { expect(subject.alpha).to eq(0.5454545454545454) }
+    it { expect(subject.beta).to eq(0.3370909090909091) }
+    it { expect(subject.gamma).to eq(0.20836363636363633) }
   end
 end
